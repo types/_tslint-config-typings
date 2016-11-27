e5dbdee46e98ab4517a7f33b8f1fa6fd0a07e5a3
Homa Wong <homawong@gmail.com>
2016-11-26 16:16:47 -0800

    Update to tslint 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
 const gulp = require('gulp');
-const lint = require('tslint');
-const tslint = require('gulp-tslint');
+const gulpTslint = require('gulp-tslint');
+const tslint = require('tslint');
 const through = require('through');
 const gutil = require('gulp-util');
 const PluginError = gutil.PluginError;
 
-var config = lint.findConfiguration();
 gulp.task('tslint-positive', function() {
   return gulp.src('spec/*.pass.ts')
-    .pipe(tslint({
-      configuration: config,
+    .pipe(gulpTslint({
+      configuration: './tslint.json',
       formatter: 'verbose'
-    }));
-    .pipe(tslint.report());
+    }))
+    .pipe(gulpTslint.report());
 });
 
 gulp.task('tslint-negative', function() {
   return gulp.src('spec/*.fail.ts')
-    .pipe(tslint({
-      configuration: config
+    .pipe(gulpTslint({
+      configuration: `./tslint.json`,
+      formatter: 'verbose'
     }))
     .pipe((function() {
       var hasError = false;
-      return through(function(file) {
+      return through(function (file) {
         if (file.tslint.failureCount === 0) {
           gutil.log(
             `[${gutil.colors.cyan('gulp-tslint')}]`,
-            gutil.colors.red('error'),
+            gutil.colors.red(`error: ${file.tslint.failureCount}`),
             `(negative) ${file.relative}`);
           hasError = true;
         }
-      }, function() {
+      }, function () {
         if (hasError) {
           this.emit('error', new PluginError('gulp-tslint', 'Failed negative test(s).'));
         } else {
